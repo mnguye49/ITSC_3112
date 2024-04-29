229224835381e639ed8e9bd6c08e8294618db875
mnguye49 <nosquishy101@gmail.com>
2024-04-29 17:12:20 -0400

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,19 +8,21 @@ public enum GameState { FreeRoam, Battle}
 
 public class GameController : MonoBehaviour
 {
-
+// fields
     [SerializeField] PlayerController playerController;
     [SerializeField] BattleSystem battleSystem;
     [SerializeField] Camera worldCamera;
 
     GameState state;
 
+// Begin program once it is executed
     private void Start()
     {
         playerController.onEncountered += StartBattle;
         battleSystem.OnBattleOver += EndBattle;
     }
 
+// Upon a random encounter playing, enter into the battle
     void StartBattle()
     {
         state = GameState.Battle;
@@ -30,6 +32,7 @@ public class GameController : MonoBehaviour
         battleSystem.StartBattle();                 // makes a new battle every encounter
     }
 
+// After a battle has been finished, return to moving around in the overworld
     void EndBattle(bool won)
     {
         state = GameState.FreeRoam;
@@ -37,6 +40,7 @@ public class GameController : MonoBehaviour
         worldCamera.gameObject.SetActive(true);
     }
 
+    // update either the player's movement or the battle
     private void Update()
     {
         if (state == GameState.FreeRoam)
